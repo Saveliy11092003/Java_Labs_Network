4ccb0420e21df80686555474fd021c7a9392353d
Saveliy <saveliy_trushkov@mail.ru>
2023-09-26 23:09:17 +0700

    First commit

---
+++ lab1_test/src/main/java/org/example/Receiver.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+public class Receiver extends Thread {
+    private AddressMap addressMap;
+    private InetAddress groupAddress;
+
+    private int port;
+
+    public Receiver(String groupIP, int port, AddressMap addressMap) throws IOException {
+        // initializing members
+        this.port = port;
+        this.groupAddress = InetAddress.getByName(groupIP);
+
+
+        this.addressMap = addressMap;
+    }
+
+    @Override
+    public void run() {
+        try (MulticastSocket socket = new MulticastSocket(port)) {
+            socket.joinGroup(groupAddress);
+            while (!Thread.currentThread().isInterrupted()) {
+                // while our thread is not interrupted doing some work
+
+                // creating buffer for message symbols
+                byte[] buffer = new byte[1024];
+
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+
+                try {
+                    // receiving packet with message
+                    socket.receive(packet);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                // binding receive-time with sender's address in addressMap
+                addressMap.add(packet.getSocketAddress(), System.currentTimeMillis());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ lab1_test/src/main/java/org/example/Sender.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.*;
+
+public class Sender extends Thread {
+
+    private DatagramPacket packet;
+    private int sleepTimeMs;
+
+    private InetAddress groupAddress;
+    private int port;
+    private String message;
+
+    public Sender(String groupIP, int port, String message, int sleepTimeMs) throws IOException {
+        // initializing members
+        this.groupAddress = InetAddress.getByName(groupIP);
+        this.port = port;
+        this.sleepTimeMs = sleepTimeMs;
+        this.message = message;
+    }
+
+    @Override
+    public void run() {
+        try(MulticastSocket socket = new MulticastSocket(port)) {
+            socket.joinGroup(groupAddress);
+            while (!Thread.currentThread().isInterrupted()) {
+                // while our thread is not interrupted doing some work
+                try {
+                    // sending packet with message to our group-mates
+                    packet = new DatagramPacket(message.getBytes(), message.length(), groupAddress, port);
+                    socket.send(packet);
+                    // waiting sleepTimeMs milliseconds
+                    Thread.sleep(sleepTimeMs);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
